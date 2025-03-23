14c697288764888674d218c9b84e1e8c3a34d9c4
David <dtant@agendize.com>
2025-03-23 17:09:58 +0100

    Test deploy

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -10,7 +10,6 @@ import { Label } from '@/components/ui/label'
 import { Input } from "@/components/ui/input"
 import { Separator } from '@/components/ui/separator'
 
-
 function SignInForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
